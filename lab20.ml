e99daf78978b8121ca11aa37084d74b8bf1fe95b
lwattendorf <lauren.wattendorf@gmail.com>
2020-04-23 08:38:14 -0700

    last labbbb

--- lab20.ml
+++ lab20.ml
@@ -1,40 +1,51 @@
-type image = float list list ;;
-(* images are lists of lists of floats between 0. (white) and 1. (black) *)
-type size = int * int ;;
+
 open Graphics ;;
+open Unix ;;
+open List ;;
+
+(* images are lists of lists of floats between 0. (white) and 1. (black) *)
+type image = float list list ;;
+type size = int * int ;;
   
 (* threshold thershold image -- image where pixels above the threshold
 value are black *)
-let threshold img threshold =
-  List.map  (fun row -> List.map (fun v -> if v <= threshold then 0. else 1.)
-                                 row) img
-       
-(* show the image *)
-let depict img =
-  Graphics.open_graph ""; Graphics.clear_graph ();
-  let x, y = List.length (List.hd img), List.length img in Graphics.resize_window x y;
-  let depict_pix v r c = let lvl = int_of_float (255. *. (1. -. v)) in Graphics.set_color (Graphics.rgb lvl lvl lvl);
-  plot c (y - r) in
-  List.iteri (fun r row -> List.iteri (fun c pix -> depict_pix pix r c) row) img;
-  Unix.sleep 2; Graphics.close_graph () ;;
+let threshold (img : image) (threshold : float) : image =
+ 	let checkthreshold (pixel: float) : float = 
+ 		if pixel <= threshold then 0. else 1. in
+  	map (fun row -> map checkthreshold row) img 
 
 (* dither max image -- dithered image *)
-let dither img =
-  List.map
-    (fun row ->
-     List.map
-       (fun v -> if v > Random.float 1.
-                 then 1.
-                 else 0.) row)
-    img
-    
+let dither (img : image) : image =
+	let rolldice (pixel : float) : float = 
+		if pixel > Random.float 1. then 1. else 0. in
+	map (fun row -> map rolldice row) img
+
+(* mirror image -- flip image left to right *)
+let mirror (img : image) : image =
+	map (fun row -> rev row) img
+
+(* flip image -- flip image up and down *)
+let flip (img : image) : image =
+	rev img
+
+(* show the image *)
+let depict img =
+  open_graph ""; 
+  clear_graph ();
+  let x, y = length (hd img), length img in resize_window x y;
+  let depict_pix v r c = 
+  	let lvl = int_of_float (255. *. (1. -. v)) in 
+  	set_color (rgb lvl lvl lvl);
+  	plot c (y - r) in
+  iteri (fun rowindex row -> iteri 
+  		(fun colindex pix -> depict_pix pix rowindex colindex) row) img;
+  sleep 2;
+  close_graph () ;;
+
 let mona = Monalisa.image ;;
-  
-  depict mona ;;
-    
-  let mona_threshold = threshold mona 0.75 ;;
-    depict mona_threshold ;;
-      
-    let mona_dither = dither mona ;;
-      depict mona_dither ;;
+depict mona ;;
+depict (threshold mona 0.75) ;;
+depict (dither mona) ;;
+depict (mirror mona) ;;
+depict (flip mona) ;;
            
